0d1e3eefde0a714faa766891056a72b7acae728a
jie12366 <2263509062@qq.com>
2020-01-18 00:18:44 +0800

    app第一版初步完成，安卓端bug待修复

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@ window.app = {
 	/**
 	 * netty服务器的url地址
 	 */
-	nettyServerUrl: 'ws://192.168.31.152:88/ws',
+	nettyServerUrl: 'ws://jie12366.xyz:888/ws',
 	
 	/**
 	 * tomcat服务器的url地址
 	 */
-	serverUrl: 'http://192.168.31.152:666',
+	serverUrl: 'https://jie12366.xyz:66',
+    
+    /**
+     * 默认的头像
+     */
+    defaultFace: "https://pic.feizl.com/upload/allimg/190123/gxtxjslmijsvh4m.jpg",
     
     /**
      * 消息动作类型
@@ -16,6 +21,13 @@ window.app = {
     CHAT: 2,     // 聊天信息
     SIGNED: 3,   // 消息签收
     KEEPALIVE:4, // 客户端保持心跳
+    PULL_FRIEND: 5, // 拉取最新好友列表
+    
+    /**
+     * 聊天信息发送者
+     */
+    FRIEND: 1, // 朋友
+    SELF: 2, // 自己
 	
 	isNotNull: function(str) {
 		if (str != null && str != "" && str != undefined) {
@@ -50,9 +62,12 @@ window.app = {
         // 触发好友请求页面的自定义事件，刷新好友请求列表
         var friendRequestWebview = plus.webview.getWebviewById("new-friends");
         mui.fire(friendRequestWebview, "refresh_request_list");
-        // 触发聊天列表页面的自定义事件，初始化websocket
+        // 触发聊天列表页面的自定义事件
         var chatListWebview = plus.webview.getWebviewById("chat-list");
+        // 初始化websocket
         mui.fire(chatListWebview, "init_websocket");
+        // 刷新聊天快照列表
+        mui.fire(chatListWebview, "refresh_chat_snapshot");
     },
     
 	/**
@@ -92,12 +107,29 @@ window.app = {
      */
     getFriendList: function() {
         var friensListStr = plus.storage.getItem("friendList");
-        
         if (!app.isNotNull(friensListStr)){
             return [];
         }
-        
         return JSON.parse(friensListStr);
+    },
+    
+    /**
+     * 根据friendId从联系人列表中获取联系人信息
+     * @param {Object} friendId
+     */
+    getInfoByFriendId: function(friendId) {
+        var that = this;
+        var contactList = that.getFriendList();
+        if (contactList != null) {
+            for (var i = 0; i < contactList.length; ++i) {
+                var friendItem = contactList[i];
+                if (friendItem.id == friendId) {
+                    return friendItem;
+                }
+            }
+        } else {
+            return null;
+        }
     },
     
     /**
@@ -123,7 +155,126 @@ window.app = {
     },
     
     /**
-     * 聊天内容模型
+     * 保存用户的聊天记录
+     * @param {Object} myId
+     * @param {Object} FriendId
+     * @param {Object} content
+     * @param {Object} sender 判断该消息的发送者,zs指朋友,self指自己
+     */
+    saveChatRecord: function(myId, friendId, sender, type, content) {
+        var that = this;
+        // 聊天记录唯一标识
+        var chatKey = "chat-" + myId + "-" + friendId;
+        // 从本地获取聊天记录
+        var chatRecordStr = plus.storage.getItem(chatKey);
+        var chatRecordList;
+        // 如果存在
+        if (that.isNotNull(chatRecordStr)) {
+            chatRecordList = JSON.parse(chatRecordStr);
+        } else {
+            // 如果不存在,则赋空集合
+            chatRecordList = [];
+        }
+        // 构建聊天记录对象
+        var singleMsg = new that.ChatRecord(sender, type, content);
+        // 向聊天记录集合中追加记录
+        chatRecordList.push(singleMsg);
+        // 保存聊天记录集合
+        plus.storage.setItem(chatKey, JSON.stringify(chatRecordList));
+    },
+    
+    /**
+     * 获取聊天记录集合
+     * @param {Object} myId
+     * @param {Object} friendId
+     */
+    getChatRecord: function(myId, friendId) {
+        var that = this;
+        // 聊天记录唯一标识
+        var chatKey = "chat-" + myId + "-" + friendId;
+        var chatRecordStr = plus.storage.getItem(chatKey);
+        var chatRecordList;
+        // 如果存在
+        if (that.isNotNull(chatRecordStr)) {
+            chatRecordList = JSON.parse(chatRecordStr);
+        } else {
+            // 如果不存在,则赋空集合
+            chatRecordList = [];
+        }
+        return chatRecordList;
+    },
+    
+    /**
+     * 保存聊天记录的快照, 仅仅保存每次聊天的最后一条消息
+     * @param {Object} myId
+     * @param {Object} friendId
+     * @param {Object} content
+     * @param {Object} isRead 消息是否已读
+     */
+    saveChatSnapshot: function(myId, friendId, content, isRead) {
+        var that = this;
+        // 聊天快照标识
+        var chatKey = "chat-snapshot-" + myId;
+        // 从本地获取聊天记录
+        var chatSnapshotStr = plus.storage.getItem(chatKey);
+        var chatSnapshoList;
+        // 如果存在
+        if (that.isNotNull(chatSnapshotStr)) {
+            chatSnapshoList = JSON.parse(chatSnapshotStr);
+            // 循环快照集合,并判断聊天快照是否已经存在
+            for (var i = 0; i < chatSnapshoList.length; ++i) {
+                if (chatSnapshoList[i].friendId == friendId) {
+                    // 删除已存在的聊天快照
+                    chatSnapshoList.splice(i, 1);
+                    break;
+                }
+            }
+        } else {
+            // 如果不存在,则赋空集合
+            chatSnapshoList = [];
+        }
+        // 构建聊天快照对象
+        var singleSnapshot = new that.ChatSnapshot(friendId, content, isRead);
+        // 向聊天快照集合的开头添加刚刚构建的快照
+        chatSnapshoList.unshift(singleSnapshot);
+        // 保存聊天快照集合
+        plus.storage.setItem(chatKey, JSON.stringify(chatSnapshoList));
+    },
+    
+    /**
+     * 更新聊天快照集合
+     * @param {Object} chatSnapshotList
+     */
+    updateChatSnapshot: function(myId, chatSnapshotList) {
+        // 聊天快照标识
+        var chatKey = "chat-snapshot-" + myId;
+        // 更新聊天快照
+        plus.storage.setItem(chatKey, JSON.stringify(chatSnapshotList));
+    },
+    
+    /**
+     * 获取聊天快照集合
+     * @param {Object} myId
+     */
+    getChatSnapshot: function(myId) {
+        var that = this;
+        // 聊天快照标识
+        var chatKey = "chat-snapshot-" + myId;
+        // 从本地获取聊天记录
+        var chatSnapshotStr = plus.storage.getItem(chatKey);
+        var chatSnapshoList;
+        // 如果存在
+        if (that.isNotNull(chatSnapshotStr)) {
+            chatSnapshoList = JSON.parse(chatSnapshotStr);
+        } else {
+            // 如果不存在,则赋空集合
+            chatSnapshoList = [];
+        }
+        return chatSnapshoList;
+    },
+    
+    /**
+     * 聊天内容对象模型
      * @param {Object} senderId
      * @param {Object} receiverId
      * @param {Object} msgId
@@ -139,7 +290,7 @@ window.app = {
     },
     
     /**
-     * 消息内容模型
+     * 消息内容对象模型
      * @param {Object} action
      * @param {Object} chatMsg
      * @param {Object} extend
@@ -149,4 +300,28 @@ window.app = {
         this.chatMsgBo = chatMsgBo;
         this.extend = extend;
     },
+    
+    /**
+     * 聊天记录对象模型
+     * @param {Object} content
+     * @param {Object} type
+     * @param {Object} sender
+     */
+    ChatRecord: function(sender, type, content) {
+        this.content = content;
+        this.type = type;
+        this.sender = sender;
+    },
+    
+    /**
+     * 聊天快照对象模型
+     * @param {Object} friendId
+     * @param {Object} content
+     * @param {Object} isRead
+     */
+    ChatSnapshot: function(friendId, content, isRead) {
+        this.friendId = friendId;
+        this.content = content;
+        this.isRead = isRead;
+    }
 }
